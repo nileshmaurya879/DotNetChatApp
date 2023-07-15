adf8d67614b0f6d25efac68f0b0990b95992b051
nilesh <nileshmaurya879@gmail.com>
2023-07-15 23:07:58 +0530

    initial commit

---
+++ ChatAppDotNet/Hubs/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using ChatAppDotNet.Models;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatAppDotNet.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task NewMessage(Message message)
+        {
+            await Clients.All.SendAsync("MessageReceived", message);
+        }
+    }
+}
---
+++ ChatAppDotNet/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatAppDotNet.Models
+{
+    public class Message
+    {
+        public string clientuniqueid { get; set; }
+        public string type { get; set; }
+        public string message { get; set; }
+        public DateTime date { get; set; }
+    }
+}
